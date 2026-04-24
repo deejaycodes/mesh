92f44aa12ffc34900691fe0514b89c220ee28b7e
Deji Odetayo <deji@corelay.dev>
2026-04-24 01:44:02 +0100

    feat(mesh-compose): public barrel
    Exports compose, approve, reject, and the type surface.

---
+++ packages/mesh-compose/src/index.ts
@@ -0,0 +1,8 @@
+export { compose } from "./compose.js";
+export { approve, reject } from "./review.js";
+export type {
+  ComposeAuthor,
+  ComposeDraft,
+  ComposeSpec,
+} from "./types.js";
+export type { LlmDraftShape } from "./compose.js";
